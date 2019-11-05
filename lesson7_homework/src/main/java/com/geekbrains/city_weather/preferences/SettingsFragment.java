d28b57f26a19b86bf3aa7af3c85e4f969a975434
Андрей <barcats@inbox.ru>
2019-11-05 20:36:22 +0300

    исправил непонятно откуда взявшуюся ошибку-SettingFragment должен быть public

--- lesson7_homework/src/main/java/com/geekbrains/city_weather/preferences/SettingsFragment.java
+++ lesson7_homework/src/main/java/com/geekbrains/city_weather/preferences/SettingsFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import com.geekbrains.city_weather.R;
 import androidx.preference.PreferenceFragmentCompat;
 
-class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
